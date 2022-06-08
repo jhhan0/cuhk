a3f56f5b67380b95b39fcc1762330be6d307f4b9
jhhan0 <1155128719@link.cuhk.edu.hk>
2022-06-08 14:41:26 +0900

    novel view synthesis program with enhanced features - implement full version of the algorithm where the target viewpoint can locate at any 3D position P(X, Y, Z) with X and Y are in range [-120, 120] and Z satisfies the range between 0 to infinity - support variable focal length that lies on the range from 0 to infinity - use bilinear interpolation method for four known neighbor viewpoints to compute the value of the new light ray from the target viewpoint

--- multimedia/view_synthesis/viewSynthesis.cpp
+++ multimedia/view_synthesis/viewSynthesis.cpp
@@ -30,6 +30,22 @@ struct Point2d
 	Point2d(double x_, double y_) :x(x_), y(y_) {}
 };
 
+double* getAlphaBeta(double x, double y) {
+	double* arr = (double*)malloc(sizeof(double) * 2);
+	arr[0] = x - floor(x); // alpha
+	arr[1] = ceil(y) - y; // beta
+	return arr;
+}
+
+int* getNeigborPoints(double x, double y) {
+	int* arr = (int*)malloc(sizeof(int) * 4);
+	arr[0] = floor(y) * 9 + floor(x); // bottom left
+	arr[1] = floor(y) * 9 + ceil(x); // bottom right
+	arr[2] = ceil(y) * 9 + floor(x); // top left
+	arr[3] = ceil(y) * 9 + ceil(x); // top right
+	return arr;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -45,7 +61,6 @@ int main(int argc, char** argv)
 	{
 		targetFocalLen = stod(argv[5]);
 	}
-	
 
 	vector<Bitmap> viewImageList;
 	//! loading light field views
@@ -67,85 +82,122 @@ int main(int argc, char** argv)
 		{
 			Point3d rayRGB(0, 0, 0);
 			//! resample the pixel value of this ray: TODO
+			double alpha, beta; 
+			int viewPoint_bl, viewPoint_br, viewPoint_tl, viewPoint_tr; // viewPoint Bottom Left, Bottom Right, Top Left, Top Right respectively
+			unsigned char rgb_bl[3], rgb_br[3], rgb_tl[3], rgb_tr[3]; // RGB Bottom Left, Bottom Right, Top Left, Top Right respectively
 			Point3d viewPoint3d(Vx, Vy, Vz);
-
+			
 			// Basic Requirement: target viewpoint locates on the camera array plane;
-			// i.e. the viewpoint coordinate = (X, Y, 0) where X and Y are in range [-120, 120].
+			// i.e. the viewpoint coordinate = (X, Y, 0) where X and Y are in range [-120, 120]
 			if (viewPoint3d.z == 0) 
 			{
 				Point2d viewPlane = {
 					(viewPoint3d.x / Baseline) + 4, ((240 - viewPoint3d.y) / Baseline) - 4
 				};
 
-				double alpha = viewPlane.x - floor(viewPlane.x);
-				double beta = ceil(viewPlane.y) - viewPlane.y;
+				double* values = getAlphaBeta(viewPlane.x, viewPlane.y);
+				alpha = values[0];
+				beta = values[1];
 
-				// find four neighbor viewpoints
-				int viewPoint_bl = floor(viewPlane.y) * 9 + floor(viewPlane.x); // viewPoint Bottom Left
-				int viewPoint_br = floor(viewPlane.y) * 9 + ceil(viewPlane.x); // viewPoint Bottom Right
-				int viewPoint_tl = ceil(viewPlane.y) * 9 + floor(viewPlane.x); // viewPoint Top Left
-				int viewPoint_tr = ceil(viewPlane.y) * 9 + ceil(viewPlane.x); // viewPoint Top Right
-
-				unsigned char rgb_bl[3], rgb_br[3], rgb_tl[3], rgb_tr[3]; // RGB Bottom Left, Bottom Right, Top Left, Top Right respectively
+				// find four neighbor viewpoints; index(0, 1, 2, 3) -> points(bl, br, tl, tr) respectively
+				int* points = getNeigborPoints(viewPlane.x, viewPlane.y);
 
 				// get RGB color from four neighbor viewpoints 
-				viewImageList[viewPoint_bl].getColor(c, r, rgb_bl[0], rgb_bl[1], rgb_bl[2]);
-				viewImageList[viewPoint_br].getColor(c, r, rgb_br[0], rgb_br[1], rgb_br[2]);
-				viewImageList[viewPoint_tl].getColor(c, r, rgb_tl[0], rgb_tl[1], rgb_tl[2]);
-				viewImageList[viewPoint_tr].getColor(c, r, rgb_tr[0], rgb_tr[1], rgb_tr[2]);
-
-				// Bilinear interpolation
-				unsigned char upper[3], lower[3], target[3];
-				for (int i = 0; i < 3; i++)
-				{
-					upper[i] = (1 - alpha) * rgb_tl[i] + alpha * rgb_tr[i];
-					lower[i] = (1 - alpha) * rgb_bl[i] + alpha * rgb_br[i];
-					target[i] = (1 - beta) * upper[i] + beta * lower[i];
-				};
-				// save resampled target pixel value
-				rayRGB = {
-					(double)target[0], (double)target[1], (double)target[2]
-				};
+				viewImageList[points[0]].getColor(c, r, rgb_bl[0], rgb_bl[1], rgb_bl[2]);
+				viewImageList[points[1]].getColor(c, r, rgb_br[0], rgb_br[1], rgb_br[2]);
+				viewImageList[points[2]].getColor(c, r, rgb_tl[0], rgb_tl[1], rgb_tl[2]);
+				viewImageList[points[3]].getColor(c, r, rgb_tr[0], rgb_tr[1], rgb_tr[2]);
 			}
 			// Enhanced Features: target viewpoint locates in the 3D space region with variable focal length;
-			// i.e. the viewpoint coordinate = (X, Y, Z) where X and Y are in range [-120, 120], Z >= 0 and focal length f > 0.
+			// i.e. the viewpoint coordinate = (X, Y, Z) where X and Y are in range [-120, 120], Z >= 0 and focal length f > 0
 			else 
 			{
 				// resample the value of ray of the target view
-				double viewToImage = (viewPoint3d.z / targetFocalLen);
+				double viewPointToImage = (viewPoint3d.z / targetFocalLen);
 				// find focal ratio
-				// - If ratio is less than 1, the result image will be enlarged. 
-				// - If ratio is greater that 1, the result image will be shrinked.
+				// - If ratio is less than 1, the result image will be enlarged
+				// - If ratio is greater that 1, the result image will be shrinked
 				double focalRatio = Focal_Length / targetFocalLen;
+
+				// convert a pixel index to its image coordinate (35 x 35)
+				Point2d imagePlane = {
+					((c * Image_Width) / (Resolution_Col - 1)) - 17, ((r * Image_Height) / (Resolution_Row - 1)) - 17
+				};
+				// compute intersection point on 2D view plane (8 by 8) based on the coordinate of image plane and the direction of ray of target view
+				Point2d viewPlane = {
+					((viewPoint3d.x + viewPointToImage * imagePlane.x) / Baseline) + 4, ((240 - (viewPoint3d.y + viewPointToImage * imagePlane.y)) / Baseline) - 4
+				};
+				// ray beyond the recorded light field; it is assigned as black (default value) e.g. rbg(0, 0, 0) 
+				if (viewPlane.x > 8 || viewPlane.x < 0 || viewPlane.y > 8 || viewPlane.y < 0)
+				{
+					targetView.setColor(c, r, (unsigned char)rayRGB.x, (unsigned char)rayRGB.y, (unsigned char)rayRGB.z);
+					continue;
+				}
+
 				if (focalRatio >= 1.0) 
 				{
 					double targetPixel = Resolution_Col / focalRatio;
-					int initial_Pixel = (int)(targetPixel / 2);
-					int end_Pixel = (int)(targetPixel / 2 + targetPixel); 
+					int initial_Pixel = (int)((Resolution_Col - targetPixel) / 2);
+					int end_Pixel = (int)(initial_Pixel + targetPixel);
 					if (c > initial_Pixel && c < end_Pixel && r > initial_Pixel && r < end_Pixel)
 					{
-						// convert a pixel index to its image coordinate (35 x 35)
-						Point2d imagePlane = {
-							((c * Image_Width) / (Resolution_Col - 1)) - 17, ((r * Image_Height) / (Resolution_Row - 1)) - 17
-						};
-						// compute intersection point on 2D view plane (8 by 8) based on the coordinate of image plane and the direction of ray of target view.
-						Point2d viewPlane = {
-							((viewPoint3d.x + viewToImage * imagePlane.x) / Baseline) + 4, ((240 - (viewPoint3d.y + viewToImage * imagePlane.y)) / Baseline) - 4
-						};
+						double* values = getAlphaBeta(viewPlane.x, viewPlane.y);
+						alpha = values[0];
+						beta = values[1];
+
+						// find four neighbor viewpoints; index(0, 1, 2, 3) -> points(bl, br, tl, tr) respectively
+						int* points = getNeigborPoints(viewPlane.x, viewPlane.y);
+
+						int x_idx = (int)((c - initial_Pixel) * focalRatio);
+						int y_idx = (int)((r - initial_Pixel) * focalRatio);
+
+						viewImageList[points[0]].getColor(x_idx, y_idx, rgb_bl[0], rgb_bl[1], rgb_bl[2]);
+						viewImageList[points[1]].getColor(x_idx, y_idx, rgb_br[0], rgb_br[1], rgb_br[2]);
+						viewImageList[points[2]].getColor(x_idx, y_idx, rgb_tl[0], rgb_tl[1], rgb_tl[2]);
+						viewImageList[points[3]].getColor(x_idx, y_idx, rgb_tr[0], rgb_tr[1], rgb_tr[2]);
 					}
 					else
 					{
 						targetView.setColor(c, r, (unsigned char)rayRGB.x, (unsigned char)rayRGB.y, (unsigned char)rayRGB.z);
+						continue;
 					}
 					
 				}
 				else
 				{
-					 
+					double targetPixel = Resolution_Col * focalRatio;
+					int initial_Pixel = (int)((Resolution_Col - targetPixel) / 2);
+					int end_Pixel = (int)(initial_Pixel + targetPixel);
+
+					double* values = getAlphaBeta(viewPlane.x, viewPlane.y);
+					alpha = values[0];
+					beta = values[1];
+
+					// find four neighbor viewpoints; index(0, 1, 2, 3) -> points(bl, br, tl, tr) respectively
+					int* points = getNeigborPoints(viewPlane.x, viewPlane.y);
+
+					int x_idx = initial_Pixel + (int)(c * focalRatio);
+					int y_idx = initial_Pixel + (int)(r * focalRatio);
+
+					viewImageList[points[0]].getColor(x_idx, y_idx, rgb_bl[0], rgb_bl[1], rgb_bl[2]);
+					viewImageList[points[1]].getColor(x_idx, y_idx, rgb_br[0], rgb_br[1], rgb_br[2]);
+					viewImageList[points[2]].getColor(x_idx, y_idx, rgb_tl[0], rgb_tl[1], rgb_tl[2]);
+					viewImageList[points[3]].getColor(x_idx, y_idx, rgb_tr[0], rgb_tr[1], rgb_tr[2]);
 				}
 			}
 
-
+			// Bilinear interpolation
+			unsigned char upper[3], lower[3], target[3];
+			for (int i = 0; i < 3; i++)
+			{
+				upper[i] = (1 - alpha) * rgb_tl[i] + alpha * rgb_tr[i];
+				lower[i] = (1 - alpha) * rgb_bl[i] + alpha * rgb_br[i];
+				target[i] = (1 - beta) * upper[i] + beta * lower[i];
+			};
+			// save resampled target pixel value
+			rayRGB = {
+				(double)target[0], (double)target[1], (double)target[2]
+			};
 			//! record the resampled pixel value
 			targetView.setColor(c, r, (unsigned char)rayRGB.x, (unsigned char)rayRGB.y, (unsigned char)rayRGB.z);
 		}
